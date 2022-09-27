8a09a5c5bcad8448d17d6694e2c33c55e6fc0534
Abigail Amelia Amethyst <abigail.amethyst@protonmail.com>
2022-09-27 12:36:35 +0100

    Adding initial python file and including todo tasks in it

---
+++ main.py
@@ -0,0 +1,5 @@
+# TODO: implement user input
+# TODO: implement calculation (PEMDAS inclusive)
+# TODO: implement printing input prompts and outputs
+# TODO: error checking for invalid inputs
+# TODO: unit testing
