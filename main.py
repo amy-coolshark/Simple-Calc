c6c4a64a2947c81954f07e615bacaf28f43393e6
Abigail Amelia Amethyst <abigail.amethyst@protonmail.com>
2022-09-28 17:14:04 +0100

    Created main function and added basic user input

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-# TODO: implement user input
 # TODO: implement calculation (PEMDAS inclusive)
 # TODO: implement printing input prompts and outputs
 # TODO: error checking for invalid inputs
 # TODO: unit testing
+
+# Main
+if __name__ == "__main__":
+    print("Welcome to the Simple-Calc program written for CSU33012 Software Engineering.")
+    print("This program only does calculation, subtraction and multiplication. Please do not enter any operands other "
+          "than that, otherwise the program will throw an error.")
+    inp = input("Please enter a calculation you would like the program to perform:\n")
